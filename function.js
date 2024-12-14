b8f86fb5bd9abb25c02a455df1412a51254e5b5b
Georgios Potsios <gpotsios@deloitte.gr>
2024-12-14 23:36:15 +0200

    Number Converter Utility

--- function.js
+++ function.js
@@ -1,9 +1,27 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
+
+function convrtFunction() {
+
+  let number = strip(document.getElementById('userInput').value);
+  const outputElement = document.getElementById('output');
+
+  postfixes = ['', 'k', 'Million', 'Billion', 'Trillion', 'Quadrillion', 'Quintillion', 'Sextillion', 'Septillion', 'Octillion', 'Nonillion']
+  count = 0
+  while (number >= 1000 && count < postfixes.length) {
+      number /= 1000
+      count++
+  }
+  outputElement.textContent = 'Output: ' + strip(number) +' '+ postfixes[count];
+  return number + postfixes[count];
+}
+
+function strip(number) {
+  return Math.round(parseFloat(number).toPrecision(12));
+}
