367442ff5b75d5370ce7b7aae6ed32d3de2d6981
8th Purple Wizard <gogospots@gmail.com>
2025-06-18 22:32:54 +0300

    feat: add Mr. Mine Save Decoder functionality and HTML interface (WIP)

--- function.js
+++ function.js
@@ -48,3 +48,26 @@ function showInfoPopup() {
 function hideInfoPopup() {
   document.getElementById("infoPopup").style.display = "none";
 }
+
+function mrMineSaveDecoder() {
+  const rawSave = document.getElementById("mrMineSaveInput").value;
+  const partsSave = rawSave.split("|"); //needs to be cleaned from the name
+  const saveWithoutName = partsSave.length > 1 ? partsSave[1] : rawSave;
+  var decodedSave = atob(atob(saveWithoutName));
+  const numbers = decodedSave.split("|");
+  const outputElement = document.getElementById("outputSave");
+  outputElement.innerHTML =
+    saveNameToNumber("Money", numbers[0]) +
+    saveNameToNumber("Kilometers", numbers[1]) +
+    saveNameToNumber("Depth Experience", numbers[2]) +
+    saveNameToNumber("Steam User ID", numbers[3]) +
+    saveNameToNumber("BP ID", numbers[4]) +
+    saveNameToNumber("BP ID", numbers[5]) +
+    saveNameToNumber("BP ID", numbers[6]) +
+    saveNameToNumber("Cargo ID", numbers[7]) +
+    saveNameToNumber("idk yet", numbers[8]);
+}
+
+function saveNameToNumber(name, number) {
+  return `<h3>${name}: ${number}</h3>`;
+}
