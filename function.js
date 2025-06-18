e721c393ac28a45832345568928b290acb430ceb
8th Purple Wizard <gogospots@gmail.com>
2025-06-18 21:09:54 +0300

    improved readability for convrtFunction and added popup function

--- function.js
+++ function.js
@@ -1,3 +1,4 @@
+// Function to toggle the navigation menu in responsive mode
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,21 +8,43 @@ function myFunction() {
   }
 }
 
+// Function to convert a number to a human-readable format with postfixes
 function convrtFunction() {
+  let number = strip(document.getElementById("userInput").value);
+  const outputElement = document.getElementById("output");
 
-  let number = strip(document.getElementById('userInput').value);
-  const outputElement = document.getElementById('output');
-
-  postfixes = ['', 'k', 'Million', 'Billion', 'Trillion', 'Quadrillion', 'Quintillion', 'Sextillion', 'Septillion', 'Octillion', 'Nonillion']
-  count = 0
+  postfixes = [
+    "",
+    "k",
+    "Million",
+    "Billion",
+    "Trillion",
+    "Quadrillion",
+    "Quintillion",
+    "Sextillion",
+    "Septillion",
+    "Octillion",
+    "Nonillion",
+  ];
+  count = 0;
   while (number >= 1000 && count < postfixes.length) {
-      number /= 1000
-      count++
+    number /= 1000;
+    count++;
   }
-  outputElement.textContent = 'Output: ' + strip(number) +' '+ postfixes[count];
+  outputElement.textContent =
+    "Output: " + strip(number) + " " + postfixes[count];
   return number + postfixes[count];
 }
 
 function strip(number) {
-  return Math.round(parseFloat(number).toPrecision(12));
+  return parseFloat(number).toPrecision(12);
+}
+
+// Function to toggle the visibility of the info popup
+function showInfoPopup() {
+  document.getElementById("infoPopup").style.display = "block";
+}
+
+function hideInfoPopup() {
+  document.getElementById("infoPopup").style.display = "none";
 }
